ee2fdfda0391a680002c6d0133aac310df3e0365
Oliver Singer <owsinger@outlook.com>
2022-10-13 20:39:31 +0100

    added value exceptions for negative and zero cases.

--- primes.py
+++ primes.py
@@ -7,16 +7,21 @@ def primes(number_of_primes):
     currentInt = 2
     checkingInt = 1
 
-    while number_of_primes > 0:
-        if checkingInt > 1 and (currentInt % checkingInt != 0):
-            checkingInt = checkingInt - 1
-        else:
-            if checkingInt == 1:
-                list.append(currentInt)
-                number_of_primes = number_of_primes - 1
-                currentInt = currentInt + 1
-                checkingInt = currentInt - 1
+    if number_of_primes < 0:
+        raise ValueError("value given is not valid (negative)")
+    elif number_of_primes == 0:
+        raise ValueError("value given is not valid (zero)")
+    else:
+        while number_of_primes > 0:
+            if checkingInt > 1 and (currentInt % checkingInt != 0):
+                checkingInt = checkingInt - 1
             else:
-                currentInt = currentInt + 1
-                checkingInt = currentInt - 1
-    return list
+                if checkingInt == 1:
+                    list.append(currentInt)
+                    number_of_primes = number_of_primes - 1
+                    currentInt = currentInt + 1
+                    checkingInt = currentInt - 1
+                else:
+                    currentInt = currentInt + 1
+                    checkingInt = currentInt - 1
+        return list
