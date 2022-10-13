841def1048373e0e2796492fc4e4535026d3733f
Oliver Singer <owsinger@outlook.com>
2022-10-13 20:35:15 +0100

    created primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
+
+    currentInt = 2
+    checkingInt = 1
+
+    while number_of_primes > 0:
+        if checkingInt > 1 and (currentInt % checkingInt != 0):
+            checkingInt = checkingInt - 1
+        else:
+            if checkingInt == 1:
+                list.append(currentInt)
+                number_of_primes = number_of_primes - 1
+                currentInt = currentInt + 1
+                checkingInt = currentInt - 1
+            else:
+                currentInt = currentInt + 1
+                checkingInt = currentInt - 1
     return list
